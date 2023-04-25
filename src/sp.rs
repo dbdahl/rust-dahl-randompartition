c5cc3768661092dbcfeb2ba4ecead1204ea980eb
David B. Dahl <dahl@stat.byu.edu>
2023-04-25 16:29:35 -0600

    Yes

--- src/sp.rs
+++ src/sp.rs
@@ -169,12 +169,11 @@ fn engine<D: PredictiveProbabilityFunction + Clone, T: Rng>(
             expand_counts(&mut counts, max_candidate_label + 1)
         }
         let n_marginal = counts_marginal[label_in_baseline];
-        let multiplier = (if use_experimental_specification() {
-            i as f64
+        let multiplier = if use_experimental_specification() {
+            shrinkage - n_marginal.ln()
         } else {
-            1.0
-        }) * shrinkage
-            / n_marginal;
+            shrinkage / n_marginal
+        };
         let labels_and_log_weights = parameters
             .baseline_ppf
             .log_predictive_weight(item, &candidate_labels, &clustering)
@@ -182,12 +181,22 @@ fn engine<D: PredictiveProbabilityFunction + Clone, T: Rng>(
             .map(|(label, log_probability)| {
                 let n_joint = counts[label_in_baseline][label];
                 let lp = log_probability
-                    + if n_joint > 0.0 {
-                        multiplier * n_joint
-                    } else if n_marginal == 0.0 && clustering.size_of(label) == 0 {
-                        shrinkage
+                    + if use_experimental_specification() {
+                        if n_joint > 0.0 {
+                            multiplier + n_joint.ln()
+                        } else if n_marginal == 0.0 && clustering.size_of(label) == 0 {
+                            shrinkage
+                        } else {
+                            0.0
+                        }
                     } else {
-                        0.0
+                        if n_joint > 0.0 {
+                            multiplier * n_joint
+                        } else if n_marginal == 0.0 && clustering.size_of(label) == 0 {
+                            shrinkage
+                        } else {
+                            0.0
+                        }
                     };
                 (label, lp)
             });
